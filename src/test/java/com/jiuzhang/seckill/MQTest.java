744d201d92b7b78775004279ff2b5dc054fe3b64
vivianpengnyc <yp2457@columbia.edu>
2021-09-01 07:57:10 -0400

    update

---
+++ src/test/java/com/jiuzhang/seckill/MQTest.java
@@ -0,0 +1,27 @@
+package com.jiuzhang.seckill;
+
+import com.jiuzhang.seckill.mq.RocketMQService;
+import com.jiuzhang.seckill.service.SeckillActivityService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+@SpringBootTest
+public class MQTest {
+
+    @Autowired
+    RocketMQService rocketMQService;
+    @Autowired
+    SeckillActivityService seckillActivityService;
+    @Test
+    public void sendMQTest() throws Exception {
+        rocketMQService.sendMessage("test-jiuzhang", "Hello World!" + new Date().toString());
+    }
+
+    @Test
+    public void pushSeckillInfoToRedisTest(){
+        seckillActivityService.pushSeckillInfoToRedis(19);
+    }
+}
---
+++ src/test/java/com/jiuzhang/seckill/RedisDemoTest.java
@@ -0,0 +1,50 @@
+package com.jiuzhang.seckill;
+
+import com.jiuzhang.seckill.util.RedisService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+public class RedisDemoTest {
+
+    @Resource
+    private RedisService redisService;
+
+    @Test
+    public void stockTest() {
+        redisService.setValue("stock:19", 10L);
+    }
+
+    @Test
+    public void getStockTest() {
+        String stock = redisService.getValue("stock:19");
+        System.out.println(stock);
+    }
+
+    @Test
+    public void stockDeductValidatorTest() {
+        boolean result = redisService.stockDeductValidator("stock:19");
+        System.out.println("result:" + result);
+        String stock = redisService.getValue("stock:19");
+        System.out.println("stock:" + stock);
+    }
+
+
+    @Test
+    public void revertStock() {
+        String stock = redisService.getValue("stock:19");
+        System.out.println("回滚库存之前的库存：" + stock);
+
+        redisService.revertStock("stock:19");
+
+        stock = redisService.getValue("stock:19");
+        System.out.println("回滚库存之后的库存：" + stock);
+    }
+
+    @Test
+    public void removeLimitMember() {
+        redisService.removeLimitMember(19, 1234);
+    }
+}
